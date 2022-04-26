f78290ce9ed86c979290364944b5e3d56d2ec5ff
Shlok Jadeja <200130107019@gecg28.ac.in>
2022-04-26 18:22:27 +0530

    chnages in Backgrounf Animation

--- src/App.js
+++ src/App.js
@@ -9,14 +9,8 @@ import Sponser from './Components/Sponsers';
 import Aboutus from './Components/Aboutus';
 import Footer from './Components/Footer';
 import Team from './Components/Team.js';
-<<<<<<< HEAD
-import Bloglist from './Components/BlogList';
-import Blogpage from './Components/Blogpage'
-import Blog_card from './Components/Blog_card';
-=======
 import Blogpage from './Components/Blogpage';
 import BackAnimations from './Components/BackAnimations'
->>>>>>> bce8bb9582ee52dcf740236a2dcedd3fd23a11dc
 
 
 
@@ -32,16 +26,7 @@ function App() {
   <Activities />
   <Sponser />
   <Discord />
-<<<<<<< HEAD
-  <Aboutus />
-
-<Team />
-  <Blogpage />
-  <Blog_card />
-  <Footer />
-=======
   <footer />
->>>>>>> bce8bb9582ee52dcf740236a2dcedd3fd23a11dc
 
   </>
   );
